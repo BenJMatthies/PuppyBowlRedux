6dbe190f9fe7b607a8d77320e9fd8fc09b60ad22
benjmatthies <benjmatthies@zohomail.com>
2023-09-28 00:20:26 -0600

    Not even close to working....

---
+++ src/Api.js
@@ -0,0 +1,68 @@
+// Add your cohort name to the cohortName variable below, replacing the 'COHORT-NAME' placeholder
+const cohortName = '2302-acc-pt-web-pt-b';
+// Use the APIURL variable for fetch requests
+const APIURL = `https://fsa-puppy-bowl.herokuapp.com/api/${cohortName}/`;
+
+/**
+ * GETs an array of all player objects
+ * @returns An array of all player objects.
+ */
+export const fetchAllPlayers = async () => {
+    try {
+        const response = await fetch(APIURL + "/players");
+        const players = await response.json();
+        // console.log(players);
+        return players;
+    } catch (err) {
+        console.error('Uh oh, trouble fetching players!', err);
+    }
+};
+
+/**
+ * GETs an object with data for the player with the passed ID
+ * @param playerId The database ID of the player to GET data for
+ * @returns an object with the requested player data
+ */
+export const fetchSinglePlayer = async (playerId) => {
+    try {
+        getURL = `${APIURL}/players/${playerId}`;
+        const response = await fetch(getURL);
+        const player = await response.json();
+        return player;
+    } catch (err) {
+        console.error(`Oh no, trouble fetching player #${playerId}!`, err);
+    }
+};
+
+//UNUSED
+// const addNewPlayer = async (playerObj) => {
+//     try {
+
+//     } catch (err) {
+//         console.error('Oops, something went wrong with adding that player!', err);
+//     }
+// };
+
+/**
+ * Method to DELETE a player from the database.
+ * @param playerId The database ID of the player to remove 
+ * @returns response after attempting to DELETE player
+ */
+export const removePlayer = async (playerId) => {
+    try {
+        playerURL = `${APIURL}/players/${playerId}`;
+        const response = await fetch(playerURL, {
+            method: "DELETE",
+            headers:
+            {
+                "Content-Type": "application/json",
+            }
+        });
+        return response;
+    } catch (err) {
+        console.error(
+            `Whoops, trouble removing player #${playerId} from the roster!`,
+            err
+        );
+    }
+};
--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,43 @@
-import { useState } from 'react'
-import './App.css'
+import { useEffect, useState } from 'react'
+import { fetchAllPlayers } from './Api';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [allPlayers, setAllPlayers] = useState([]);
+  const [singlePlayer, setSinglePlayer] = useState(null);
+
+  useEffect(()=>{
+    setAllPlayers = fetchAllPlayers();
+    console.log(allPlayers);
+  }, [])
+
 
   return ((
     <>
+      <RenderAllPlayers allPlayer={allPlayers} setAllPlayers={setAllPlayers}/>
     </>
   ))
+}
+
+function RenderAllPlayers({allPlayers, setAllPlayers}) {
+  
+  
+  <return>(
+    {allPlayers.map(player => <RenderPlayerAll player={player} />)}
+    )</return>
+
+}
+
+function RenderPlayerAll({ player }) {
+
+  return (
+    <div className='player'>
+      <h2>Name: {player.name}</h2>
+      <p>Breed: {player.breed}</p>
+      <p>Team: {player.teamId}</p>
+      <button className='details-button'>See Details</button>
+      <button className='delete-button'>Delete Player</button>
+    </div>
+  )
 }
 
 export default App
